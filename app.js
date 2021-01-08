2861a877d929d0cfd157f06c137d276260e4f54e
pradhanharshil <harshilpradhan11@gmail.com>
2021-01-08 14:39:14 +0530

    Added API Key

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ window.addEventListener('load', () => {
     let long;
     let lat;
     let iconCode;
-    const apiBaseURL = "http://api.openweathermap.org/data/2.5/weather";
-    const iconBaseURL = "http://openweathermap.org/img/wn/";
+    const apiBaseURL = "https://api.openweathermap.org/data/2.5/weather";
+    const iconBaseURL = "https://openweathermap.org/img/wn/";
     const apiKey = "891d097726f76d30bc558152a31469c9";
 
     const cityName = document.getElementById("city");
@@ -38,7 +38,7 @@ window.addEventListener('load', () => {
                 temperatureDescription.textContent = description.charAt(0).toUpperCase() + description.slice(1);
                 unit.textContent = "C";
             }).catch((error) => {
-                alert(error.message);
+                console.log(error.message);
             });
         });
     } else {
